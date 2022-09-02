b7a23275d15b36e40b8b1bbcba58437c098bb829
Ahmad Yaghi <ahmad.abdallah1993@outlook.com>
2022-09-03 01:25:29 +0300

    done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,8 +4,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 class App extends React.Component {
   constructor(props) {
@@ -20,7 +20,8 @@ class App extends React.Component {
       cardFlag: false,
       dataFlag: false,
       weath: [],
-      photoArray: []
+      photoArray: [],
+      mv:[]
     };
   }
 
@@ -30,75 +31,141 @@ class App extends React.Component {
     const cityName = event.target.city.value;
     const key = "pk.d63f789567346be0d16e65b136ea44aa";
     const URL = `https://us1.locationiq.com/v1/search?key=${key}&q=${cityName}&format=json`;
-    let resultResponce;
-    try {
-      resultResponce = await axios.get(URL);
-      // console.log(resultResponce.data[0].lat);
+    // let resultResponce;
+    // try {
+    //   resultResponce = await axios.get(URL);
+    //   // console.log(resultResponce.data[0].lat);
 
-      this.setState({
-        display_name: resultResponce.data[0].display_name,
-        lat: resultResponce.data[0].lat,
-        lon: resultResponce.data[0].lon,
+    //   this.setState({
+    //     display_name: resultResponce.data[0].display_name,
+    //     lat: resultResponce.data[0].lat,
+    //     lon: resultResponce.data[0].lon,
+    //     correctFlag: true,
+    //     cardFlag: true,
+    //     errorFlag: false,
+    //   });
+    // } catch {
+    //   this.setState({
+    //     errorFlag: true,
+    //     correctFlag: false,
+    //     cardFlag: false,
+    //   });
+    // }
+
+
+    axios.get(URL).then(result => {
+        this.setState({
+        display_name: result.data[0].display_name,
+        lat: result.data[0].lat,
+        lon: result.data[0].lon,
         correctFlag: true,
         cardFlag: true,
         errorFlag: false,
       });
-    } catch {
-      this.setState({
-        errorFlag: true,
-        correctFlag: false,
-        cardFlag: false,
-      });
-    }
-    this.weather(resultResponce.data[0].lat, resultResponce.data[0].lon);
-    this.siplashSplash(cityName)
+      this.weather(result.data[0].lat, result.data[0].lon);
+      this.siplashSplash(result.data[0].display_name);
+      this.moPoster(cityName)
+    }).catch(error =>{
+      console.log(error)
+    })
+
+
+
+
+
+   
 
     //ahmad
   };
 
-  weather = async (lat, lon) => {
+
+
+
+  moPoster = (item) =>{
+    const URL = `http://localhost:3005/movie?searchMovie=${item}`;
+
+    axios.get(URL).then(result =>{
+      console.log(result.data)
+      this.setState({
+        mv:result.data
+      })
+    }).catch(error =>{
+      console.log(error)
+    })
+  }
+
+
+
+
+
+
+
+
+
+
+
+  // async
+  weather = (lat, lon) => {
     const url = `http://localhost:3005/weatherLocalApi?lat=${lat}&lon=${lon}`;
-    try {
-      const result = await axios.get(url);
+    // try {
+    //   const result = await axios.get(url);
+    //   this.setState({
+    //     dataFlag: true,
+    //     weath: result.data,
+    //   });
+    //   console.log(result.data);
+
+    //   // console.log('****************************',typeof result.data);
+    //   // let a = JSON.stringify(result.data)
+    //   // return a.map( item => item)
+
+    //   //  result.data.map( item => {
+    //   //     return (this.setState({
+    //   //       dataFlag: true,
+    //   //       weath: item
+    //   //     }))
+    //   //   })
+
+    //   // console.log('---------------------',this.state.weath[0].date)
+    // } catch {}
+
+
+    axios.get(url).then(result =>{
       this.setState({
         dataFlag: true,
         weath: result.data,
       });
-      console.log(result.data);
+    }).catch( error =>{
+      console.log(error)
+    })
 
-      // console.log('****************************',typeof result.data);
-      // let a = JSON.stringify(result.data)
-      // return a.map( item => item)
 
-      //  result.data.map( item => {
-      //     return (this.setState({
-      //       dataFlag: true,
-      //       weath: item
-      //     }))
-      //   })
 
-      // console.log('---------------------',this.state.weath[0].date)
-    } catch {}
   };
 
+  //async
+  siplashSplash = (n) => {
+    const URl = `http://localhost:3005/splashPhoto?photos=${n}`;
 
-  siplashSplash = async (n) => {
-    const URl = `http://localhost:3005/splashPhoto?photos=${n}`
-    
-    try{
-      const gPhoto = await axios.get(URl);
+    // try {
+    //   const gPhoto = await axios.get(URl);
+    //   this.setState({
+    //     photoArray: gPhoto.data,
+    //   });
+    //   console.log(gPhoto.data);
+    // } catch {}
+
+
+    axios.get(URl).then(result =>{
       this.setState({
-        photoArray: gPhoto.data
-      })
-      console.log(gPhoto.data)
-    }catch{
-      
+            photoArray: result.data,
+          });
+    }).catch(error =>{
+      console.log(error)
+    })
 
-    }
-    
 
-  }
-
+  };
 
   render() {
     return (
@@ -149,11 +216,11 @@ class App extends React.Component {
             {this.state.weath.map((item) => {
               return (
                 <>
-                <br></br>
+                  <br></br>
                   <Card.Text>Date: {item.date}</Card.Text>
                   <Card.Text>Description: {item.description}</Card.Text>
                   <br></br>
-                  </>
+                </>
               );
             })}
           </Card>
@@ -162,36 +229,44 @@ class App extends React.Component {
         <br></br>
 
         <>
-        
-      
-        {this.state.photoArray.map( item => {
-          return (<Row xs={1} md={4} className="g-4">
-          {Array.from({ length: 1 }).map((_, idx) => (
-          <Col>
-          <Card>
-            <Card.Img variant="top" src={item.url} />
-            <Card.Body>
-              <Card.Title></Card.Title>
-              <Card.Text>
-                {item.likes}
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          
-        </Col>
-        
-      ))}
-       </Row>)
-        })}
-       
-        
+          {this.state.photoArray.map((item) => {
+            return (
+              <Row xs={1} md={4} className="g-4">
+                {Array.from({ length: 1 }).map((_, idx) => (
+                  <Col>
+                    <Card>
+                      <Card.Img variant="top" src={item.url} />
+                      <Card.Body>
+                        <Card.Title></Card.Title>
+                        <Card.Text>Number of likes: {item.likes}</Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+            );
+          })}
         </>
-
-
-
-
-
-
+        <>
+          {this.state.mv.map((item) => {
+            return (
+              <Row xs={1} md={1} className="g-4">
+                {Array.from({ length: 1 }).map((_, idx) => (
+                  <Col>
+                    <Card>
+                      <Card.Title>{item.title}</Card.Title>
+                      <Card.Img variant="top" src={item.backdrop_path} />
+                      <Card.Body>
+                        <Card.Title></Card.Title>
+                        <Card.Text>Number of likes: {item.overview}</Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+            );
+          })}
+        </>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -7,16 +7,12 @@ require("dotenv").config();
 const weatherData = require("./weather.json");
 const { default: axios } = require("axios");
 
-
 const PORT = process.env.PORT || 3005;
-
-
 
 // http://localhost:3005/
 server.get("/", (req, res) => {
   console.log("helllllo from home route");
 });
-
 
 // http://localhost:3005/weatherLocalApi?LAT=PUT_LATITUDE&LON=PUT-LON
 server.get("/weatherLocalApi", (req, res) => {
@@ -34,53 +30,89 @@ server.get("/weatherLocalApi", (req, res) => {
     ///console.log(weatherArr); //to check it in terminal
     //res.state(200).send(weatherArr);
     res.send(weatherArr);
-  } catch {
-
-  }
+  } catch {}
 });
-
-
 
 // https://api.unsplash.com/search/photos?query=amman&client_id=78SAdbKiselbecZPc5yPsPVZKOsoVBMbdNgQfdOpaOw
 
 // http://localhost:3005/splashPhoto?photos=cars
-server.get('/splashPhoto', getSplashPhotoHandler);
+server.get("/splashPhoto", getSplashPhotoHandler);
 
-async function getSplashPhotoHandler(req,res) {
+function getSplashPhotoHandler(req, res) {
   const searchPhotos = req.query.photos;
   console.log(searchPhotos); //to see the searchPhotos to the terminal
-  const URL = `https://api.unsplash.com/search/photos?query=${searchPhotos}&client_id=${process.env.key}`
+  const URL = `https://api.unsplash.com/search/photos?query=${searchPhotos}&client_id=${process.env.key}`;
   console.log(URL); //to see the url in terminal
 
+//   try {
+//     const apiResult = await axios.get(URL);
+//     // res.send(apiResult.data);
+//     let arrPhoto = apiResult.data.results.map((item) => {
+//       return new SplashP(item);
+//     });
+//     console.log(arrPhoto);
+//     res.send(arrPhoto);
+//   } catch {}
 
-  try{
-    const apiResult = await axios.get(URL);
-    // res.send(apiResult.data);
-    let arrPhoto = apiResult.data.results.map( item =>{
+
+
+axios.get(URL).then(result =>{
+    let arrPhoto = result.data.results.map((item) => {
       return new SplashP(item);
-    })
-    console.log(arrPhoto);
+    });
     res.send(arrPhoto);
+}).catch(error =>{
+  res.send(error)
+})
 
-  }catch{
 
+
+ }
+
+ // https://api.themoviedb.org/3/search/movie?api_key=516a315cbe724b34d8db26e97561e390&query=Jack+Reacher
+
+// http://localhost:3005/movie?searchMovie=amman
+ server.get('/movie', getMovieHandeler);
+
+ function getMovieHandeler(req,res){
+  let moviii = req.query.searchMovie;
+  const URL = `https://api.themoviedb.org/3/search/movie?api_key=516a315cbe724b34d8db26e97561e390&query=${moviii}`;
+  axios.get(URL).then(result =>{
+    // console.log(result.data.results);
+
+    let arr = result.data.results.map( item =>{
+      return new Mov(item);
+    })
+    console.log(arr)
+    res.send(arr);
+
+
+  }).catch(error =>{
+    console.log(error)
+  })
+
+ }
+
+
+class Mov {
+  constructor(item){
+    this.title = item.title;
+    this.overview = item.overview;
+    this.backdrop_path = item.backdrop_path;
   }
-
-
-
-
 }
 
 
-class SplashP{
-  constructor(item){
+
+
+
+
+class SplashP {
+  constructor(item) {
     this.url = item.urls.regular;
     this.likes = item.likes;
   }
 }
-
-
-
 
 // function Forcast(item){
 
